946faaaa116f9abcdb84a197a54684fce147a8fc
Manuel Sopena Ballesteros <masber@hotmail.com>
2026-01-02 19:28:06 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use futures::io::BufReader;
+use futures::AsyncBufRead;
+use std::pin::Pin;
+
+// This function belongs to a different crate to talk to k8s API
+pub async fn get_logs() -> anyhow::Result<impl AsyncBufRead> {
+    Ok(BufReader::new("Hello, world!".as_bytes()))
+}
+
+// The trait to get logs from a backend that has a k8s cluster
+trait K8sLoger {
+    type T: AsyncBufRead;
+
+    async fn get_log_buffer() -> anyhow::Result<Self::T>;
+}
+
+// K8s backend
+struct Backend1 {}
+
+impl K8sLoger for Backend1 {
+    type T = Pin<Box<dyn AsyncBufRead>>;
+
+    async fn get_log_buffer() -> anyhow::Result<Self::T> {
+        Ok(Box::pin(get_logs().await?))
+    }
+}
+
+// Journalctl backend
+struct Backend2 {}
+
+impl K8sLoger for Backend2 {
+    type T = Pin<Box<dyn AsyncBufRead>>;
+
+    async fn get_log_buffer() -> anyhow::Result<Self::T> {
+        Ok(Box::pin(get_logs().await?))
+    }
+}
+
+// Function to get the logs from any backend
+pub async fn get_system_logs(backend_type: &str) -> anyhow::Result<impl AsyncBufRead> {
+    match backend_type {
+        "backend1" => Backend1::get_log_buffer().await,
+        "backend2" => Backend2::get_log_buffer().await,
+        &_ => panic!("Not supported"),
+    }
+}
+
+pub fn main() {
+    let backend_type: &str = "backend1";
+    get_system_logs(backend_type);
+}
